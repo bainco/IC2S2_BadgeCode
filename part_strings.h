3e5f227c743b87d6363d32289cddcf0f29f44532
Connor Bain <bainco@email.sc.edu>
2016-06-16 19:35:08 -0500

    Updated folder structure.
    Added check-in server code.

--- part_strings.h
+++
@@ -1,5 +0,0 @@
-#define MY_ID ((short) 499)
-const char MY_NAME[16] = "BATMAN";
-
-// THESE NEED TO BE THE INDICES INTO THE ANSWER ARRAYS
-const unsigned short MY_ANSWERS[NUM_QUESTIONS] = {1, 1, 1, 1};
