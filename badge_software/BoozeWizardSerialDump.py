d55bbc517ff669f9f0be488abe76cc211e93dca5
Connor Bain <bainco@email.sc.edu>
2016-06-20 15:59:35 -0500

    Updated BoozeWizard protocol.

--- badge_software/BoozeWizardSerialDump.py
+++ badge_software/BoozeWizardSerialDump.py
@@ -127,11 +127,13 @@ try:
             print 'IDs: ' + str(badge_ids)
 
         # Dump to file
-        f = open('data-' + str(theirID) + '.txt', 'w')
+        #f = open('data-' + str(theirID) + '.txt', 'w')
+        f = open('data.csv', 'a')
         f.write('MY_ID\n' + str(theirID) + '\n')
         f.write('MY_DATA\n')
         for i in xrange(0, num_records):
             f.write(str(badge_times[i]) + ',' + str(badge_ids[i]) + '\n')
+        f.write('\n')
         f.close()
 
 except Exception, err:
