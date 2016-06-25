79d2fbacccc34d583236fc5b5b3ed404862f5474
Connor Bain <bainco@email.sc.edu>
2016-06-24 23:37:04 -0500

    Added workshop template.

---
+++ badge_workshop_template/eeprom.c
@@ -0,0 +1,155 @@
+/**
+EEPROM.c
+@Author Connor Bain
+@Copyright Connor Bain, 2016
+
+This file contains all necessary function definitons and constants for the
+IC2S2 conference badges to be able to burn and retrieve contacts to/from EEPROM.
+*/
+
+#include "simpletools.h"   // Include simpletools library
+#include "badgetools.h"    // Include badgetools library
+#include "eeprom.h"        // Include EEPROM header
+#include "ID_Info.h"       // Include ID info header
+/**** WRITE METHODS ****/
+
+/** storeUserCount(short count)
+
+Method to store the user interaction count.
+
+@args count (short) to store in memory
+*/
+void storeUserCount(short count) {
+  ee_writeShort(count, USER_COUNT_ADDRESS);
+}
+
+/** storeServerCount(short count)
+
+Method to store the server interaction count.
+
+@args count (short) to store in memory
+*/
+void storeServerCount(short count) {
+  ee_writeShort(count, SERVER_COUNT_ADDRESS);
+}
+
+/** storeContact(int timeVal, short idVal)
+
+Method to store an interaction into EEPROM.
+
+@args timeVal (int) timestamp of interaction
+      idVal (short) the id of the interactant
+
+@returns address (int) of next open memory slot
+*/
+int storeContact(int timeVal, short idVal) {
+   // Calculate next open memory slot
+   unsigned int addr = MEM_START_ADDRESS + (retrieveCount() * 6);
+   short newCount;
+   // Simple bounds check
+   if(addr < (TIME_ADDRESS - 6)) {
+      ee_writeInt(timeVal, addr);
+      addr += 4;
+      ee_writeShort(idVal, addr);
+      addr += 2;
+      //int memRemaining = 65535 - (addr);
+      //memRemaining /= 6;
+
+      // Cases based on the ID values
+      if (idVal < STAFF_ID_UPPER_LIMIT) {
+         // THIS IS A STAFF BADGE
+         // STUB
+      }
+      else if (idVal < USER_ID_UPPER_LIMIT) {
+         // THIS IS A USER BADGE
+         newCount = retrieveUserCount();
+         newCount = newCount + 1;
+         storeUserCount(newCount);
+      }
+      else if (idVal < SERVER_ID_UPPER_LIMIT) {
+         // THIS IS A SERVER BADGE
+         newCount = retrieveServerCount();
+         newCount = newCount + 1;
+         storeServerCount(newCount);
+      }
+   }
+   // Otherwise, we've got a out of memory problem
+   else {
+      clear();
+      oledprint("EEPROM ERROR!\n");
+      pause(1000);
+   }
+   return addr;
+}
+/** END WRITE METHODS **/
+
+
+/******** READ METHODS *********/
+
+/** retrieveUserCount()
+
+Method to retrieve the user interaction count.
+
+@returns number (short) of user interactions
+*/
+short retrieveUserCount() {
+   // If no count is stored, initialize
+   if (ee_readShort(USER_COUNT_ADDRESS) == -1) {
+     storeUserCount(0);
+     return 0;
+   }
+   return ee_readShort(USER_COUNT_ADDRESS);
+}
+
+/** retrieveServerCount()
+
+Method to retrieve the server interaction count.
+
+@returns number (short) of server interactions
+*/
+short retrieveServerCount() {
+
+   // If no count is stored, initialize
+   if (ee_readShort(SERVER_COUNT_ADDRESS) == -1) {
+      storeServerCount(0);
+      return 0;
+   }
+   return ee_readShort(SERVER_COUNT_ADDRESS);
+}
+
+/** retrieveCount()
+
+Method to retrieve the total interaction count. Just calls the other two methods.
+
+@returns number (short) of total interactions
+*/
+short retrieveCount() {
+   short newCount;
+   newCount = retrieveServerCount();
+   newCount = newCount + retrieveServerCount();
+  return retrieveServerCount() + retrieveUserCount();
+}
+
+/** retrieveContact(unsigned int addr, int *timeVal, short *idVal)
+
+Method to retrieve a contact from EEPROM.
+
+@args addr (unsigned int) of the contact in EEPROM
+      timeVal (*int) a pointer to a variable to store the retrieved time
+      idVal (*short) a pointer to a variable to store the retrieved ID
+
+@returns address (unsigned int) of next contact
+*/
+unsigned int retrieveContact(unsigned int addr, int *timeVal, int *idVal) {
+  if(addr < (TIME_ADDRESS - 6)) {
+    *timeVal = ee_readInt(addr);
+    addr += 4;
+    *idVal = (int) ee_readShort(addr);
+    addr += 2;
+  }
+  else {
+    clear();
+    oledprint("ERROR!\n");
+  }
+  return addr;
+}
---
+++ badge_workshop_template/participant-code.c
@@ -0,0 +1,553 @@
+/**
+participant-code.c for Workshop
+@Author Connor Bain
+@Copyright Connor Bain, 2016
+
+This class contains all of the main code for the IC2S2 electronic conference
+badges. It serves as the template for all conference attendee badges and includes
+the main menu, check-in, part-to-part comms, and saving data to EEPROM.
+*/
+
+#include "simpletools.h"                     // Include simpletools library
+#include "badgetools.h"                      // Include badgetools library
+#include "datetime.h"                        // Include datetime library
+#include "eeprom.h"                          // Include EEPROM library
+#include "survey.h"                          // Include Survey library
+#include "ID_Info.h"                         // Include ID info
+#include "part_strings.h"                    // Include Part Info
+
+/*** GLOBALS ***/
+short LISTENFORSERVER_TIMEOUT = 100;
+
+int *cog = 0;
+
+// Variables for Time
+datetime dt;                                  // Date/teme values
+int MY_TIME;                                  // Epoch time value
+char dates[9];                                // Date string
+char times[9];                                // Time string
+
+// Variables for Comms
+int serverTime;
+int serverID;
+
+int theirID;
+int theirAnswers[NUM_QUESTIONS];
+char theirName[32];
+/** END GLOBALS **/
+
+/** FUNCTION DEFINITIONS **/
+// Comm Methods
+void sendBeacon();
+void listenForServer();
+
+// Button Methods
+void All_OSH_Erase_EEPROM();
+void P17_Check_In();
+void P27_PartToPart();
+
+// Display Methods
+void Display_Main_Menu();
+void myScroll();
+void Display_Private_SumStats(unsigned int *y);
+void apply_user_edits();
+
+// Helper Methods
+void maybe_kill_cog();
+/** END FUNCTION DEFS **/
+
+
+/*********************************************************/
+/*********************************************************/
+/*************** WORSHOP WORKSPACE *******************/
+/*********************************************************/
+/*********************************************************/
+void apply_user_edits() {
+
+// Apply your edits to the home page here!
+
+// To invert your screen (replace all black pixels with white pixels)
+// invert(1);
+// To revert, use invert(0);
+
+// To rotate your screen, use rotate180();
+}
+
+// Look for the next workshop workspace in the main() loop!
+
+/*********************************************************/
+/*********************************************************/
+/*************** END WORSHOP WORKSPACE *******************/
+/*********************************************************/
+/*********************************************************/
+
+
+/****** MAIN LOOP *****/
+void main() {
+  badge_setup();                             // Call badge setup
+
+  MY_TIME = ee_readInt(TIME_ADDRESS);        // Get most recent time
+
+  if (MY_TIME == -1) {
+      dt = dt_fromEt(MY_INIT_TIME);                         // Convert from epoch to datetime
+      ee_writeInt(dt_toEt(dt), TIME_ADDRESS);
+  }
+
+  else {
+      dt = dt_fromEt(MY_TIME);
+  }
+
+  dt_run(dt);                                 // Use to start system timer
+
+  Display_Main_Menu();
+
+  int states;
+  int y;
+  // Main loop for button pressing
+  while(1) {
+    // Update time
+    dt = dt_get();                             // Get current system time
+    MY_TIME = dt_toEt(dt);                     // Datetime -> epoch time
+    dt_toDateStr(dt, dates);                   // Convert to strings
+    dt_toTimeStr(dt, times);
+
+    // BUTTON LOGIC
+    states = buttons();
+    switch(states) {
+
+/*********************************************************/
+/*********************************************************/
+/*************** WORSHOP WORKSPACE ***********************/
+/*********************************************************/
+/*********************************************************/
+      // P15 Pressed
+      case 0b0001000:
+         /** DO NOT THIS LINE **/
+         maybe_kill_cog();
+         /** DO NOT ABOVE LINE **/
+
+         // Fill in your P15 code here!
+
+         /** DO NOT REMOVE THIS LINE **/
+         Display_Main_Menu();
+         /** DO NOT REMOVE ABOVE LINE **/
+         break;
+
+      case 0b0010000:
+         /** DO NOT REMOVE THIS LINE **/
+         maybe_kill_cog();
+         /** DO NOT REMOVE ABOVE LINE **/
+
+         // Fill in your P16 code here!
+
+         /** DO NOT REMOVE THIS LINE **/
+         Display_Main_Menu();
+         /** DO NOT REMOVE ABOVE LINE **/
+         break;
+
+      // P25 Pressed
+      case 0b0000100:
+         /** DO NOT REMOVE THIS LINE **/
+         maybe_kill_cog();
+         /** DO NOT REMOVE ABOVE LINE **/
+
+         // Fill in your P25 code here!
+
+         /** DO NOT REMOVE THIS LINE **/
+         Display_Main_Menu();
+         /** DO NOT REMOVE ABOVE LINE **/
+         break;
+
+      // P26 Pressed
+      case 0b0000010:
+         /** DO NOT REMOVE THIS LINE **/
+         maybe_kill_cog();
+         /** DO NOT REMOVE ABOVE LINE **/
+
+         // Fill in your P26 code here!
+
+         /** DO NOT REMOVE THIS LINE **/
+         Display_Main_Menu();
+         /** DO NOT REMOVE ABOVE LINE **/
+         break;
+/*********************************************************/
+/*********************************************************/
+/*************** END WORSHOP WORKSPACE *******************/
+/*********************************************************/
+/*********************************************************/
+
+      case 0b0100000:
+        if (cog != 0) {
+         cog_end(cog);
+         cog = 0;
+        }
+        screen_auto(ON);
+        P17_Check_In();                       // P17 pressed, Server comms
+        break;
+
+      case 0b0000001:                         // P27 pressed, Part Comms
+        if (cog != 0) {
+         cog_end(cog);
+         cog = 0;
+        }
+        screen_auto(ON);
+        P27_PartToPart();
+        break;
+
+      case 0b1111111:                         // OSH pressed, erase EEPROM
+        if (cog != 0) {
+         cog_end(cog);
+         cog = 0;
+        }          screen_auto(ON);
+        All_OSH_Erase_EEPROM();
+        break;
+
+       default:                               // otherwise, check for private mode
+         y = accel(AY);
+          if (y < -40) {
+            if (cog != 0) {
+              cog_end(cog);
+              cog = 0;
+            }
+            screen_auto(ON);
+            Display_Private_SumStats(&y);
+          }
+        // Save time to EEPROM every minute
+        if(dt.s == 0) {                             // If next minute over
+           MY_TIME = dt_toEt(dt);                   // Datetime -> epoch time
+           ee_writeInt(MY_TIME, TIME_ADDRESS);      // Store in EEPROM
+           led(0, ON);                              // Indicate storing P27 LED
+           while(dt.s == 0) {                       // Wait for second 0 to finish
+             dt = dt_get();                         // to avoid multiple EE writes
+         }
+         led(0, OFF);                               // Second over, P27 LED off
+      }
+        break;
+    }
+  }
+}
+
+/***** BUTTON METHODS ******/
+void P17_Check_In() {
+   clear();
+   oledprint("Check-in Mode");
+   irclear();
+   listenForServer();
+   rgbs(OFF, OFF);
+   Display_Main_Menu();
+}
+
+void P27_PartToPart() {
+
+   // Prep to receive
+   char received = 0;
+   int irlenb = 0;
+   memset(&theirID, 0, sizeof(theirID));
+   memset(&theirAnswers, 0, sizeof(theirAnswers));
+   irclear();
+
+   // Display to User
+   clear();
+   text_size(SMALL);
+   oledprint("Hold still\nplease!");
+
+   // Send info via IR
+   rgbs(GREEN, GREEN);
+   pause(1000);
+   rgbs(RED, RED);
+   clear();
+   for (int i = 0; i < 2; i++) {
+      oledprint("%d,%d,%d,%d,%d\n", MY_ID, MY_ANSWERS[0], MY_ANSWERS[1], MY_ANSWERS[2], MY_ANSWERS[3]);
+      irprint("%d,%d,%d,%d,%d", MY_ID, MY_ANSWERS[0], MY_ANSWERS[1], MY_ANSWERS[2], MY_ANSWERS[3]);
+      pause(20);
+   }
+   rgbs(OFF, OFF);
+   pause(1000);
+
+   // Look into IR Buffer for Response
+   irlenb = irscan("%d,%d,%d,%d,%d", &theirID, &theirAnswers[0], &theirAnswers[1], &theirAnswers[2], &theirAnswers[3]);
+
+ rgbs(RED, RED);
+   clear();
+   for (int i = 0; i < 2; i++) {
+      oledprint("%d,%d,%d,%d,%d\n", MY_ID, MY_ANSWERS[0], MY_ANSWERS[1], MY_ANSWERS[2], MY_ANSWERS[3]);
+      irprint("%d,%d,%d,%d,%d", MY_ID, MY_ANSWERS[0], MY_ANSWERS[1], MY_ANSWERS[2], MY_ANSWERS[3]);
+      pause(20);
+   }
+   rgbs(OFF, OFF);
+
+   if (irlenb > 0) {
+      rgbs(CYAN, CYAN);
+      received = 1;
+      pause(200);
+      rgbs(OFF, OFF);
+   }
+
+   // If we got a message, do some processing
+   if (received == 1) {
+      // SAVE THE CONTACT
+      dt = dt_get();
+      MY_TIME = dt_toEt(dt);
+      storeContact(MY_TIME, (short) theirID);
+
+      // Pick a random question
+      // Either picks a similarity or worst-case dissim.
+      short randIndex = rand() % NUM_QUESTIONS;
+      if (MY_ANSWERS[randIndex] != theirAnswers[randIndex]) {
+         for (short i = 0; i < NUM_QUESTIONS; i++) {
+            if (MY_ANSWERS[i] ==  theirAnswers[i]) {
+               randIndex = i;
+               break;
+            }
+         }
+      }
+
+      // Display results
+      clear();
+      text_size(SMALL);
+      // Display dissim
+      if (MY_ANSWERS[randIndex] != theirAnswers[randIndex]) {
+                   //S              E
+         oledprint("They answered   ");
+      }
+      // Display sim
+      else {     //S              E
+         oledprint("Both answered   ");
+      }
+
+      oledprint("%32s\n", SURVEY_Q_ANSWERS[randIndex][theirAnswers[randIndex]]);
+      oledprint(" to:\n");
+      oledprint(SURVEY_Q[randIndex]);
+      rotate180();   // Flip for private mode
+
+      pause(10000);
+   }
+   // Otherwise, no message received
+   else {
+      clear();
+      oledprint("No message      received.\n");
+      oledprint("Please try again");
+      pause(2000);
+   }
+   irclear();
+   Display_Main_Menu();
+}
+
+// Method to erase EEPROM.
+// You need to hold down 7 butons, so this should never happen.
+void All_OSH_Erase_EEPROM() {
+  clear();
+  oledprint("CONFIRM");
+  pause(1000);
+
+  int states = buttons();
+
+  text_size(SMALL);
+  cursor(0, 4);
+  oledprint("Yes-OSH\n\n");
+  oledprint("No-Any other btn");
+  while(!(states = buttons()));         // Get confirmation
+  if(states == 0b1000000)               // Yes, erase
+  {
+     text_size(LARGE);
+     clear();
+     oledprint("Wait 20s...");
+     contacts_eraseAll();
+     oledprint("Done!");
+     pause(250);
+     Display_Main_Menu();
+   }
+  else                                   // No, don't erase
+  {
+     pause(250);
+     Display_Main_Menu();
+  }
+}
+
+/** HELPER METHODS **/
+
+// Method to transmit all contacts via IR
+void irTxAll() {
+   // Comm variables
+   int aTime;
+   int anID;
+
+   unsigned int address = MEM_START_ADDRESS;    // Get ready to read
+   short currentCount = retrieveCount();        // Get the current count
+
+   // For every contact
+   for(int i = 0; i < currentCount; i++) {
+      // Clear their variables
+      memset(&aTime, 0, sizeof(aTime));
+      memset(&anID, 0, sizeof(anID));
+
+      address = retrieveContact(address, &aTime, &anID); // Grab contact from memory
+
+      rgbs(RED, RED);
+      clear();
+      text_size(SMALL);
+      oledprint("SENDING...%d\n", i);
+      oledprint("%d\n%d\n", aTime, anID);
+      irprint("%d,%d", aTime, anID);
+      pause(500);
+      rgbs(OFF, OFF);
+   }
+   // Send terminate command
+   clear();
+   text_size(SMALL);
+   oledprint("TERMINATE.");
+   irprint("%d,%d", (int) TERMINATE, (int) TERMINATE);
+   pause(1000);
+}
+
+// Method to wait for a sever beacon until timeout
+void listenForServer() {
+   short i = 0;
+   int irlenb = 0;
+   while(i < LISTENFORSERVER_TIMEOUT) {
+      rgbs(GREEN, GREEN);
+      // Clear their variables
+      memset(&serverID, 0, sizeof(serverID));
+      memset(&serverTime, 0, sizeof(serverTime));
+      memset(&theirName, 0, sizeof(theirName));
+
+      irlenb = irscan("%d,%d,%32s", &serverTime, &serverID, &theirName);
+      if(strlen(theirName) > 0) {
+         irclear();
+         rgbs(CYAN, CYAN);
+         clear();
+         text_size(SMALL);
+         oledprint("ID: %d\n", serverID);
+         oledprint("Time: %d\n", serverTime);
+         oledprint("Name: %32s", theirName);
+         pause(50);
+
+         // Special case for BoozeWizard
+         if (serverID == BOOZE_WIZARD_ID) {
+            clear();
+            text_size(LARGE);
+            oledprint("HOLD\nSTILL");
+            rgbs(RED, RED);
+            irprint("%d,%d,%32s", MY_ID, dt_toEt(dt), MY_FIRST_NAME);
+            pause(1000);
+            // Send beacon to BoozeWizard
+            clear();
+            irTxAll();  // Transmit all data
+            rgbs(OFF, OFF);
+            return;  // Done, leave the method
+         }
+
+         // Otherwise, either TimeWizard or Server
+         dt_set(dt_fromEt(serverTime));   // Update time to server time
+
+         // Store interaction, send server beacon
+         if (serverID != TIME_WIZARD_ID) {
+            clear();
+            text_size(SMALL);
+            oledprint("Check-in at\n%d", serverID);
+            storeContact(serverTime, (short) serverID);
+            sendBeacon();
+         }
+         // Otherwise, this is just a time update
+         else {
+            clear();
+            text_size(SMALL);
+            oledprint("Time Update!");
+         }
+         pause(500);
+         rgbs(OFF, OFF);
+         return;
+      }
+      pause(50);
+      rgbs(OFF, OFF);
+      i += 1; // Continue listen loop
+   }
+   // If we're here, we never got anything.
+   rgbs(RED, RED);
+   clear();
+   text_size(SMALL);
+   oledprint("No check-in.\nPlease try again");
+   pause(1000);
+   rgbs(OFF, OFF);
+   return;
+}
+
+// Method to send an IR beacon to the server
+void sendBeacon() {
+   for(int i = 0; i < 3; i++) {
+      rgbs(RED, RED);
+      irprint("%d,%d,%32s", MY_ID, dt_toEt(dt), MY_FIRST_NAME);
+
+      clear();
+      text_size(SMALL);
+      oledprint("%d\n%d\n%32s\n", MY_ID, dt_toEt(dt), MY_FIRST_NAME);
+      pause(500);
+      rgbs(OFF, OFF);
+   }
+}
+
+/**** DISPLAY METHODS ******/
+void myScroll() {
+   screen_auto(OFF);
+   char length = strlen(MY_FIRST_NAME);
+   char position = 0;
+   while (1) {
+      for (char j = 0; j < 8; j++) {
+         cursor(j, 0);
+         oledprint("%c", MY_FIRST_NAME[(position + j) % length]);
+         cursor(j, 1);
+         oledprint("%c", MY_LAST_NAME[(position + j) % length]);
+      }
+      position = (position + 1) % length;
+      screen_update();
+      pause(25);
+      if (position == 1) pause(5000);
+   }
+}
+
+void Display_Private_SumStats(unsigned int *y) {
+   clear();
+   text_size(SMALL);
+   screen_auto(OFF); // Disable update while we filp the screen
+            //S              ES              E
+   oledprint("You've checked  in at %d places.\n\n", retrieveServerCount());
+   oledprint("You've met\n %d people!\n", retrieveUserCount());
+   rotate180();
+   screen_update();  // UPDATE
+
+   // While in private mode, keep doin
+   while (*y < -40) {
+    *y = accel(AY);
+    pause(20);
+   }
+   // Exit
+   clear();
+   screen_auto(ON);  // Renable screen auto-update
+   Display_Main_Menu();
+}
+void Display_Main_Menu() {
+   clear();
+   text_size(LARGE);
+
+   if ((strlen(MY_FIRST_NAME) > 8) || (strlen(MY_LAST_NAME) > 8)) {
+      cog = cog_run(&myScroll, 128);
+   }
+
+   else {
+      cursor(0, 0);
+      oledprint("%s", MY_FIRST_NAME);
+      cursor(0, 1);
+      oledprint("%s", MY_LAST_NAME);
+   }
+   apply_user_edits();
+}
+/***** END DISPLAY METHODS *****/
+
+/*** Helper Function ***/
+void maybe_kill_cog() {
+   if (cog != 0) {
+    cog_end(cog);
+    cog = 0;
+  }
+  screen_auto(ON);
+}
