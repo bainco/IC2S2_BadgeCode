3137aec1d8cdda92d4650a5602bae4541b28fc06
Connor Bain <bainco@email.sc.edu>
2021-09-11 18:03:21 -0500

    some more missing stuff

--- badge_software/part_strings.h
+++ badge_software/part_strings.h
@@ -1,5 +1,5 @@
 const int MY_INIT_TIME = 1466843396;
 const int MY_ID = 1416;
-const char MY_FIRST_NAME[32] = "Vudtiwat        ";
-const char MY_LAST_NAME[32] = "Ngampruetikorn  ";
+const char MY_FIRST_NAME[32] = "Connor";
+const char MY_LAST_NAME[32] =  "Bain";
 const int MY_ANSWERS[NUM_QUESTIONS] = {1,0,4,1};
--- checkin_server/ic2s2-badgeregistration/export_as_csv.py
+++ checkin_server/ic2s2-badgeregistration/export_as_csv.py
@@ -7,26 +7,26 @@ from main import Attendee
 from datetime import datetime, timedelta
 
 def exportToCsv(query, csvFileName, delimiter):
-	with open(csvFileName, 'wb') as csvFile:
-		totalRowsSaved = 0
-		csvWriter = csv.writer(csvFile, delimiter=delimiter, quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		writeHeader(csvWriter)
+    #with open(csvFileName, 'wb') as csvFile:
+    totalRowsSaved = 0
+    csvWriter = 0 #csv.writer(csvFile, delimiter=delimiter, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writeHeader(csvWriter)
 
-		items = query.fetch()
-		currentRows =0
-		for item in items:
-			saveItem(csvWriter, item)
-			currentRows += 1
+    items = query.fetch()
+    currentRows =0
+    for item in items:
+        saveItem(csvWriter, item)
+        currentRows += 1
 
-		totalRowsSaved += currentRows
-		print 'Saved ' + str(totalRowsSaved) + ' rows'
-		print 'Finished saving all rows.'
+    totalRowsSaved += currentRows
+    print 'Saved ' + str(totalRowsSaved) + ' rows'
+    print 'Finished saving all rows.'
 
 def writeHeader(csvWriter):
-	csvWriter.writerow(['date', 'badgeID', 'firstName', 'lastName', 'email', 'affiliation', 'position', 'discipline', 'degree','sonicq_0', 'sonicq_1', 'sonicq_2', 'sonicq_3', 'sonicq_4', 'sonicq_5', 'survey_answer0', 'survey_answer1', 'survey_answer2', 'survey_answer3']) #Output csv header
+	print(['date', 'badgeID', 'firstName', 'lastName', 'email', 'affiliation', 'position', 'discipline', 'degree','sonicq_0', 'sonicq_1', 'sonicq_2', 'sonicq_3', 'sonicq_4', 'sonicq_5', 'survey_answer0', 'survey_answer1', 'survey_answer2', 'survey_answer3']) #Output csv header
 
 def saveItem(csvWriter, item):
-	csvWriter.writerow([item.date, item.badgeID, item.firstName, item.lastName, item.email, item.affiliation, item.position, item.discipline, item.degree, item.sonicq_0, item.sonicq_1, item.sonicq_2, item.sonicq_3, item.sonicq_4, item.sonicq_5, item.survey_answer0, item.survey_answer1, item.survey_answer2, item.survey_answer3]) # Save items in preferred format
+	print([item.date, item.badgeID, item.firstName, item.lastName, item.email, item.affiliation, item.position, item.discipline, item.degree, item.sonicq_0, item.sonicq_1, item.sonicq_2, item.sonicq_3, item.sonicq_4, item.sonicq_5, item.survey_answer0, item.survey_answer1, item.survey_answer2, item.survey_answer3]) # Save items in preferred format
 
 query = Attendee.gql("ORDER BY date", produce_cursors=True) #Query for items
 
